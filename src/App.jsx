7d13f9f58614f2b322e71f243c37f88769088e76
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-03-25 23:05:10 -0400

    Add GameBoards component for wrapping multiple game boards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
-import Keyboard from './Components/Keyboard';
-import Board from './Components/Board';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+// import { GameContextProvider } from './context/GameContext';
+// import { CurrentGuessContextProvider } from './context/CurrentGuessContext';
+// import { GameContext, GuessContext } from './helpers/contexts';
 import words5 from './data/962-5-letter-words';
 import scrabbleWords from './data/172820-scrabble-words';
-import Navbar from './Components/Navbar';
+
+import Navbar from './components/Navbar';
+import GameBoards from './components/GameBoards';
+import Keyboard from './components/Keyboard';
 
 const StyledApp = styled.div`
   width: 100vw;
@@ -18,7 +21,7 @@ const StyledApp = styled.div`
   align-items: center;
 `;
 
-const AppWrapper = styled.div`
+const GameWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin: 1rem;
@@ -27,25 +30,10 @@ const AppWrapper = styled.div`
   align-items: center;
 `;
 
-const BoardWrapper = styled(motion.div)`
-position: relative;
-width: 100%;
-display: flex;
-gap: 2rem;
-max-height: calc(var(--vh, 1vh) * 60);
-overflow-x: scroll;
-scrollbar-width: none;
-&::-webkit-scrollbar {
-    width: 0 !important
-  }
-`;
-
-const words = getWords(words5);
+const answers = getAnswers(words5);
+console.log('answers: ', answers);
 
 function App() {
-  const [allGuesses, setAllGuesses] = useState([]);
-  const [curGuess, setCurGuess] = useState('');
-  const [curBoard, setCurBoard] = useState(0);
 
   const setAppHeight = () => {
     // Set CSS vh variable to window innerheight. 
@@ -53,22 +41,33 @@ function App() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
-
   useEffect(() => {
     setAppHeight();
     window.onresize = setAppHeight;
   }, []);
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyPress);
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  });
+  // Game Context
+  const [gameIsActive, setGameIsActive] = useState(true);
+  const [submittedGuesses, setSubmittedGuesses] = useState([]);
+  // const gameContextValue = { answers, gameIsActive, setGameIsActive, submittedGuesses, setSubmittedGuesses };
 
-  const handleKeyPress = (e) => {
+  // Guess Context
+  const [curGuesses, setCurGuesses] = useState(['', '', '']);
+  const [guessing, setGuessing] = useState(0);
+
+  useEffect(() => {
+    for (let i = 0; i < answers.length; i++) {
+      if (curGuesses[i].length < answers[i].length) {
+        setGuessing(i);
+        break;
+      }
+    }
+  }, [curGuesses]);
+
+  function handleKeyPress(e) {
+    if (!gameIsActive) return;
     if (65 <= e.keyCode && e.keyCode <= 90) {
-      addLetter(e.key);
+      addLetter(e.key.toUpperCase());
     } else if (e.keyCode === 8) {
       removeLetter();
     } else if (e.keyCode === 13) {
@@ -76,63 +75,97 @@ function App() {
     }
   };
 
-  const addLetter = letter => {
-    if (curGuess.length >= 15) return;
-    if ([5, 10].includes(curGuess.length)) {
-      const lastWord = curGuess.slice(-5).toLowerCase();
-      if (!scrabbleWords.includes(lastWord)) return; 
+  function addLetter(letter) {
+    const curGuess = curGuesses[guessing] || '';
+
+    // Do nothing if all tiles are full
+    if (curGuesses.join('').length >= answers.join('').length) return;
+
+    // If starting new word
+    if (!curGuess.length && guessing > 0) {
+
+      // Only start new word if previous one is valid
+      const lowerWord = curGuesses[guessing - 1].toLowerCase();
+      if (!scrabbleWords.includes(lowerWord)) return;
     }
-    setCurGuess(prev => prev + letter.toUpperCase());
+
+    setCurGuesses(prev => {
+      const newGuesses = [...prev];
+      newGuesses[guessing] = prev[guessing] + letter;
+      return newGuesses;
+    });
   };
-  const removeLetter = () => {
+
+
+  function removeLetter() {
+    let curGuess = curGuesses[guessing];
+    let curGuessing = guessing;
+
+    // Move to previous guess if current is already empty
+    if (!curGuess.length && guessing > 0) {
+      curGuessing--;
+      curGuess = curGuesses[curGuessing];
+    }
+
+    // If nothing typed, return
     if (!curGuess.length) return;
-    setCurGuess(prev => prev.slice(0, -1));
-  };
-  const submitGuess = () => {
-    const guessWords = curGuess.match(/.{1,5}/g).map(w => w.toLowerCase());
-    if (curGuess.length !== 15) return;
-    if (guessWords.some(w => !scrabbleWords.includes(w))) return;
-    setCurGuess('');
-    setAllGuesses(prev => [...prev, curGuess]);
+
+    const newGuesses = [...curGuesses];
+    newGuesses[curGuessing] = curGuess.slice(0, -1);
+    setCurGuesses(newGuesses);
   };
 
+  function submitGuess() {
+    console.log('submitting guess');
+    const curGuessesStr = curGuesses.join('');
+    const answersStr = answers.join('');
 
-  const boards = [0, 1, 2].map(i => {
-    const startInd = i * 5;
-    const endInd = startInd + 5;
-    const word = words.slice(startInd, endInd);
-    const guess = curGuess.slice(startInd, endInd);
-    const guesses = allGuesses.map(guess => guess.slice(startInd, endInd));
-    const key = i;
-    return <Board {...{ key, word, guesses, guess }} />;
+    // Do nothing if current guesses incomplete
+    if (curGuessesStr.length !== answersStr.length) return 'Wrong number of letters';
+    log(1)
+    // Do nothing if any words aren't valid
+    if (curGuesses.some(w => !scrabbleWords.includes(w.toLowerCase()))) return 'Some word not valid';
+    log(2)
+    
+    // If guesses are correct, end game
+    if (curGuessesStr === answersStr) setGameIsActive(false);
+    log(3)
+
+    const guessesToSubmit = [...curGuesses]
+    setCurGuesses(['','','']);
+    setSubmittedGuesses(prev => [...prev, curGuesses]);
+    return 'Submitted';
+  };
+
+  const guessContextValue = { curGuesses, guessing, addLetter, removeLetter, submitGuess };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => document.removeEventListener('keydown', handleKeyPress);
   });
-
-  const keyGuesses = ['hello', 'array'];
-  const keyWord = words.slice(curBoard * 5, 5 + curBoard * 5);
-
 
   return (
     <StyledApp>
       <Navbar />
-      <AppWrapper>
-        <BoardWrapper
-          animate={{}}
-        >
-          {boards}
-        </BoardWrapper>
-        <Keyboard {...{ word: keyWord, guesses: keyGuesses, addLetter, removeLetter, submitGuess }} />
-      </AppWrapper>
+      <GameWrapper>
+        <GameBoards {...{ answers, submittedGuesses, curGuesses }} />
+        <Keyboard {...{ answers, submittedGuesses, guessing, addLetter, removeLetter, submitGuess }} />
+      </GameWrapper>
     </StyledApp>
   );
 }
 
 export default App;
 
-function getWords(wordsArray) {
+function getAnswers(wordsArray) {
   const words = [];
   for (let i = 0; i < 3; i++) {
     let word = wordsArray[Math.floor(Math.random() * wordsArray.length)].toUpperCase();
     words.push(word);
   }
-  return words.join('');
+  return words;
+}
+
+function log(name, thing = ' ') {
+  console.log(`${name}: `, thing);
 }
---
+++ src/Components/GameBoards.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+import Board from './Board';
+
+const BoardsWrapper = styled(motion.div)`
+width: 100%;
+display: flex;
+gap: 2rem;
+max-height: calc(var(--vh, 1vh) * 60);
+overflow-x: scroll;
+scrollbar-width: none;
+&::-webkit-scrollbar { width: 0 !important }
+`;
+
+function GameBoards(props) {
+  return (
+    <BoardsWrapper>
+      {props.answers.map((answer, i) => {
+        return <Board
+          key={i}
+          boardIndex={i}
+          {...{ answer, ...props }}
+        />;
+      })}
+    </BoardsWrapper>
+  );
+}
+
+export default GameBoards;
