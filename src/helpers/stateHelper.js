02715f6306046e4c3f84c4f72baa6343a5287990
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-03-29 09:50:34 -0400

    Add logic for assessing tile states

--- src/helpers/stateHelper.js
+++ src/helpers/stateHelper.js
@@ -3,30 +3,129 @@
 class Game {
   constructor(answers, guessRows) {
     this.answers = answers;
-    this.guesses = guesses;
+    this.guessRows = guessRows;
 
-    this.tileRows = guessRows
-      .map((guessRow, rowIndex) => {
-        return new TileRow(this, guessRow, rowIndex)
-      });
-  }
-}
-
-class TileRow {
-  constructor(game, guessRow, rowIndex) {
-    this.game = game;
-    this.tiles = guessRow.map((guess, boardIndex) => {
-      return guess
+    // 2D Array of answer letters
+    // for tracking what's known 
+    // about each through the game.
+    // States: unknown, hinted, presence, location
+    this.answerStates = answers
+      .map((answer, boardIndex) => answer
         .split('')
-        .map((letter, letterIndex) => {
-          return new Tile(letter, answers, boardIndex, rowIndex, letterIndex);
+        .map((letter, letterIndex) => { 
+          return new AnswerState({ 
+          letter, 
+          answer, 
+          letterIndex, 
+          boardIndex
+          }); 
+        })
+      );
+
+    // Make 3D Array of tiles [guessRows [singleGuesses [letters] ] ]
+    this.tiles = guessRows.map((guessRow, rowIndex) => {
+      return guessRow.map((guess, boardIndex) => {
+        return guess.split('').map((letter, letterIndex) => {
+          return new Tile(this, letter, boardIndex, rowIndex, letterIndex);
         });
+      });
     });
+  }
+  evaluateGuessRow(rowIndex) {
+
+    
+    
+    const guessRow = this.guessRows[rowIndex];
+    const rowTiles = this.tiles[rowIndex];
+    
+    // Main guess loop
+    guessRow.forEach((guess, bIndex) => {
+      const gAnswerStates = this.answerStates[bIndex]
+
+      const guessTiles = rowTiles[bIndex];
+      // const aStates = rowAnswerStates[bIndex];
+      const answer = this.answers[bIndex];
+
+      // Set state for correct letters & 
+      // create arrays of letters still not found
+      const unaccountedFor = gAnswerStates
+        .filter((aLetterObj, lIndex) => {
+          const tile = guessTiles[lIndex];
+
+          if (aLetterObj.letter === guess[lIndex]) {
+            // Set answerLetter's state to confirmed and
+            // don't include in lettersNotFoundFromWords
+            aLetterObj.knownState = 'location'
+            tile.state = 'correct'
+            return false;
+          }
+          return true;
+        });
+
+      // Set state for letters out of place
+      guess.split('').forEach((guessLetter, lIndex) => {
+        const tile = guessTiles[lIndex];
+        const letterUnaccountedFor = unaccountedFor.includes(guessLetter);
+        if (letterUnaccountedFor && aStates[lIndex] === 'hidden') {
+          aStates[lIndex] = 'present';
+          const foundIndex = unaccountedFor.indexOf(guessLetter);
+          unaccountedFor.splice(foundIndex, 1);
+        }
+      });
+
+
+
+    });
+
+  }
+  getGuessWord(boardIndex, rowIndex) {
+    return this.tiles[rowIndex][boardIndex];
   }
 }
 
 class Tile {
-  constructor(letter, answers, boardIndex, rowIndex, letterIndex) {
-    this.
+  constructor(game, letter, boardIndex, rowIndex, letterIndex) {
+    this.game = game;
+    this.letter = letter;
+    this.boardIndex = boardIndex;
+    this.rowIndex = rowIndex;
+    this.letterIndex = letterIndex;
+
+    this.answerWord = answers[boardIndex];
+    this.answerLetter = this.answerWord[letterIndex];
+    this.state = 'absent'; // States: absent, elsewhere, present, correct
+  }
+  tileName() {
+    return `Tile B${this.boardIndex}-R${this.rowIndex}-L${letterIndex}`;
+  }
+}
+
+class AnswerState {
+  constructor(props) {
+    this.letter = props.letter, 
+    this.answer = props.answer, 
+    this.letterIndex = props.letterIndex, 
+    this.boardIndex = props.boardIndex, 
+    this.knownState = 'unknown' // unknown, hinted, presence, location
+  }
+  know(state) {
+    const prev = this.knownState;
+    if (state === 'hinted') {
+
+      if (['location', 'presence'].includes(prev)) return;
+      this.knownState = 'hinted'
+
+    } else if (state === 'presence') {
+
+      if (prev === 'location') return;
+      this.knownState = 'presence';
+      
+    } else if (state === 'location') {
+
+      this.knownState = 'location';
+
+    } else {
+      console.error('Invalid state');
+    }
   }
 }
