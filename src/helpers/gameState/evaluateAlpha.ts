31f0d264e1892dc6c2301e3002c25d19672866e5
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-08-29 08:00:06 -0400

    Add code comments to problematic evaluateAlpha game logic

--- src/helpers/gameState/evaluateAlpha.ts
+++ src/helpers/gameState/evaluateAlpha.ts
@@ -3,6 +3,13 @@ import { TileEvaluation } from "./evaluateGuessRow";
 export type AlphaEvaluation = ('unknown' | 'absent' | 'hinted' | 'present' | 'correct');
 export type AlphaMap = Map<string, AlphaEvaluation[]>;
 
+
+/**
+ * 
+ * @param guessRows - A 2D array of 'guess rows', each an array containing three guess strings
+ * @param guessEvals - A 3D array of 'guess rows', each an array containing three arrays of 'tile evaluation' strings
+ * @returns 
+ */
 export default function evaluateAlpha(
   guessRows: string[][],
   guessEvals: TileEvaluation[][][]
@@ -36,19 +43,25 @@ export default function evaluateAlpha(
 
         const prev = alphaArr[bIndex]
         switch (guessEval) {
+          // If ever an eval is 'correct', set the letter on this board to correct.
           case 'correct':
             alphaArr[bIndex] = 'correct';
             break;
+          // If ever an eval is 'present', set the letter on this board to 'present' unless it's already set to 'correct'.
           case 'present':
             if (prev === 'correct') break;
             alphaArr[bIndex] = 'present';
             break;
+          // If ever an eval is 'absent', set the letter on this board to 'absent' unless it's already set to 'correct' or 'present'.
+          // Also, if all boards for this letter are currently labelled 'absent' or 'unknown', set all boards to 'absent'. 
           case 'absent':
             if (prev === ('correct' || 'present')) break;
             alphaArr[bIndex] = 'absent';
             if (alphaArr.some(s => s === ('correct' || 'present' || 'hinted'))) break;
             alphaArr.fill('absent');
             break;
+          // If ever an eval is 'elsewhere', add it to 'tilesLabelledElsewhere' to revisit it later.
+          // Also, set the letter on this board to 'absent'.
           case 'elsewhere':
             tilesLabelledElsewhere.push([rIndex, bIndex, lIndex]);
             alphaArr[bIndex] = 'absent';
